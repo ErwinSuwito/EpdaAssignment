5d989af3ddfab5dee8c51f5a9c8fdc7e819ecf11
Erwin Suwitoandojo <30384397+ErwinSuwito@users.noreply.github.com>
2022-02-20 15:01:20 +0700

    Refactored Customer class to use String for id

--- EpdaAssignment-ejb/src/java/model/Customer.java
+++ EpdaAssignment-ejb/src/java/model/Customer.java
@@ -21,17 +21,50 @@ public class Customer implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String email;
+    private String id;
     private String password;
     private String name;
     private String phoneNumber;
 
-    public Long getId() {
+    public Customer(String id, String password, String name, String phoneNumber) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Customer() {
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
