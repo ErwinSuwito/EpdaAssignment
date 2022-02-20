f3835293852d718959a143ff11fff44167a5eb1f
Erwin Suwitoandojo <30384397+ErwinSuwito@users.noreply.github.com>
2022-02-20 15:53:17 +0700

    Added code to add product to orders

--- EpdaAssignment-war/src/java/controller/orders/AddItemToCart.java
+++ EpdaAssignment-war/src/java/controller/orders/AddItemToCart.java
@@ -7,11 +7,21 @@ package controller.orders;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.ejb.EJB;
+import javax.persistence.criteria.Order;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.Customer;
+import model.OrderProduct;
+import model.OrderProductFacade;
+import model.Orders;
+import model.Product;
+import model.ProductFacade;
 
 /**
  *
@@ -19,6 +29,12 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "AddItemToCart", urlPatterns = {"/AddItemToCart"})
 public class AddItemToCart extends HttpServlet {
+
+    @EJB
+    private OrderProductFacade orderProductFacade;
+
+    @EJB
+    private ProductFacade productFacade;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -32,17 +48,30 @@ public class AddItemToCart extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        
+        HttpSession session = request.getSession(false);
+        Customer customer = (Customer)session.getAttribute("customerLogin");
+        
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AddItemToCart</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AddItemToCart at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            if (customer == null) {
+                // TO-DO: Redirect to customer login page
+            } else {
+                Orders order = (Orders)session.getAttribute("cart");
+                if (order == null) {
+                    order = new Orders();
+                }
+                
+                String productId = request.getParameter("productId");
+                int quantity = Integer.parseInt(request.getParameter("quantity"));
+                Product product = productFacade.find(productId);
+                OrderProduct orderProduct = new OrderProduct(order, product, quantity);
+                
+                order.getProductBasket().add(orderProduct);
+                
+                request.setAttribute("cart", order);
+                
+                // TO-DO: Redirect to product added to cart page
+            }
         }
     }
 
